7f63b1fe37988bb39aaca244b4c08be793b9e504
Carlos <ccorcobado@gmail.com>
2019-10-20 17:57:12 +0200

    Clean code

--- mastermind/types/TypeView.java
+++ mastermind/types/TypeView.java
@@ -1,14 +1,7 @@
 package mastermind.types;
 
 public enum TypeView {
+	
     CONSOLE;
 
-    public static TypeView get(String string) {
-        for (TypeView vType : TypeView.values()) {
-            if (vType.name().equalsIgnoreCase(string)) {
-                return vType;
-            }
-        }
-        return TypeView.CONSOLE;
-    }
 }
